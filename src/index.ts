c6017e7a80e3d2561ae171feeff19b6f4ffb8975
Raul Melo <melo.raulf@gmail.com>
2024-01-28 12:25:17 +0100

    add meow and consola to better handle input/output

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { parseArgs } from "util";
+import meow from "meow";
+import consola from "consola";
+
 import { NodeVersions } from "./schema";
 
 const response = await fetch("https://nodejs.org/dist/index.json");
@@ -7,44 +9,77 @@ const nodeVersionsJson = await response.json();
 
 const nodeVersions = NodeVersions.parse(nodeVersionsJson);
 
-const { values: argValues } = parseArgs({
-	args: Bun.argv,
-	options: {
-		lts: {
-			type: "boolean",
-			default: false,
-		},
-		latest: {
-			type: "boolean",
-			default: false,
+const { flags } = meow(
+	`Usage
+$ node-versions <flags>
+
+Options
+--lts   Show all LTS versions
+--all   Show all versions
+
+Examples
+$ node-versions
+v21.1
+
+$ node-versions --lts
+v20.11.0
+v20.10.0
+v20.9.0
+v18.19.0
+v18.18.2
+...
+
+$ node-versions --all
+v21.6.1
+v21.6.0
+v21.5.0
+v21.4.0
+v21.3.0
+v21.2.0
+v21.1.0
+v21.0.0
+v20.11.0
+...
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			lts: {
+				type: "boolean",
+				default: false,
+			},
+			all: {
+				type: "boolean",
+				default: false,
+			},
 		},
 	},
-	strict: true,
-	allowPositionals: true,
-});
+);
 
-if (argValues.lts) {
+if (flags.lts) {
 	showLts();
-} else if (argValues.latest) {
+} else if (flags.all) {
+	showAll();
+} else {
 	const [latestVersions] = nodeVersions;
 	logVersions([latestVersions]);
-} else {
-	showAll();
 }
 
 function showAll() {
-	console.log("All Versions:");
+	consola.log("All Versions:");
 	logVersions(nodeVersions);
 }
 
 function showLts() {
-	console.log("LTS Versions:");
+	consola.info("LTS Versions:");
 	const ltsVersions = nodeVersions.filter((version) => version.lts);
 	logVersions(ltsVersions);
 }
 
 function logVersions(nodeVersions: NodeVersions) {
-	for (const nodeVersion of nodeVersions) {
-		console.log(nodeVersion.version);
-	}
+	const result = nodeVersions.reduce((acc, nodeVersion) => {
+		return `${acc}\n${nodeVersion.version}`;
+	}, "");
+
+	consola.log(result);
 }
