ae83ad272967807e4dff064b48e97aab4d889eb7
Raul Melo <melo.raulf@gmail.com>
2024-01-28 12:09:43 +0100

    add show lts flag

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { parseArgs } from "util";
 import { NodeVersions } from "./schema";
 
 const response = await fetch("https://nodejs.org/dist/index.json");
@@ -6,4 +7,30 @@ const nodeVersionsJson = await response.json();
 
 const nodeVersions = NodeVersions.parse(nodeVersionsJson);
 
-console.log(nodeVersions);
+const { values: argValues } = parseArgs({
+	args: Bun.argv,
+	options: {
+		lts: {
+			type: "boolean",
+			default: false,
+		},
+	},
+	strict: true,
+	allowPositionals: true,
+});
+
+if (argValues.lts) {
+	showLts();
+}
+
+function showLts() {
+	console.log("LTS Versions:");
+	const ltsVersions = nodeVersions.filter((version) => version.lts);
+	logVersions(ltsVersions);
+}
+
+function logVersions(nodeVersions: NodeVersions) {
+	for (const nodeVersion of nodeVersions) {
+		console.log(nodeVersion.version);
+	}
+}
